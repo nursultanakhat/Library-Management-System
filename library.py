39a0870d532b5c19586f4fa531f99887281e04e9
user <250812@astanait.edu.kz>
2026-05-10 16:58:45 +0500

    Week 3: Added user registration and search feature

--- library.py
+++ library.py
@@ -3,13 +3,36 @@ import json
 class Library:
     def __init__(self):
         self.books = []
+        self.users = []
 
     def add_book(self, title, author):
-        self.books.append({"title": title, "author": author})
+        self.books.append({
+            "title": title,
+            "author": author
+        })
 
     def show_books(self):
+        if len(self.books) == 0:
+            print("No books in library.")
+
+        else:
+            for book in self.books:
+                print(book)
+
+    def register_user(self, name):
+        self.users.append(name)
+        print("User registered successfully!")
+
+    def search_book(self, keyword):
+        found = False
+
         for book in self.books:
-            print(book)
+            if keyword.lower() in book["title"].lower() or keyword.lower() in book["author"].lower():
+                print(book)
+                found = True
+
+        if not found:
+            print("Book not found.")
 
     def save(self):
         with open("data.json", "w") as f:
--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-print("START")
 from library import Library
 
 lib = Library()
 
-lib.add_book("Harry Potter", "Rowling")
-lib.show_books()
-lib.save()
+while True:
+    print("\nLibrary Management System")
+    print("1. Add book")
+    print("2. Show books")
+    print("3. Register user")
+    print("4. Search book")
+    print("5. Save data")
+    print("6. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        title = input("Enter book title: ")
+        author = input("Enter book author: ")
+
+        lib.add_book(title, author)
+
+        print("Book added successfully!")
+
+    elif choice == "2":
+        lib.show_books()
+
+    elif choice == "3":
+        name = input("Enter user name: ")
+
+        lib.register_user(name)
+
+    elif choice == "4":
+        keyword = input("Enter title or author: ")
+
+        lib.search_book(keyword)
+
+    elif choice == "5":
+        lib.save()
+
+        print("Data saved successfully!")
+
+    elif choice == "6":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option.")
